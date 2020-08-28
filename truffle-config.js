2d9c50ccd37cad23422506afe7735db7f0f83424
Jikun Liao <liaojikundever@gmail.com>
2020-08-27 20:04:56 -0500

    Deploy to rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -1,22 +1,36 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider")
-const {privateKey, publicKey} = require("./privatekey")
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { privateKey, publicKey } = require("./privatekey");
 
 module.exports = {
   networks: {
     development: {
-     host: "127.0.0.1",
-     port: 8545,
-     network_id: "*",
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*",
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(
+        privateKey,
+        "https://rinkeby.infura.io/v3/86b8675879914aab9dc6263b77e930f7"
+      ),
+      network_id: 4,
+      skipDryRun: true,
+      from: publicKey,
+      networkCheckTimeout: "10000000",
+      gasPrice: 201e9,
     },
     live: {
       provider: function() {
-        return new HDWalletProvider(privateKey, "https://mainnet.infura.io/v3/a07aaaeb8e1c4277a1d9354b5b420548")
+        return new HDWalletProvider(
+          privateKey,
+          "https://mainnet.infura.io/v3/a07aaaeb8e1c4277a1d9354b5b420548"
+        );
       },
       network_id: 1,
       gasPrice: 81e9,
       from: publicKey,
-      gas: 8e6
-    }
+      gas: 8e6,
+    },
   },
 
   compilers: {
@@ -24,12 +38,12 @@ module.exports = {
       version: "0.5.16",
       docker: false,
       settings: {
-       optimizer: {
-         enabled: true,
-         runs: 200
-       },
-       evmVersion: "byzantium"
-      }
-    }
-  }
-}
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+        evmVersion: "byzantium",
+      },
+    },
+  },
+};
